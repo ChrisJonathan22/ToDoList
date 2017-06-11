10d3854e645a19b8f946a93eaffe87253e884dc6
ChrisJonathan22 <christophereko@hotmail.fr>
2017-06-11 15:55:14 +0100

    ToDoList
    Simple ToDoList. Made with HTML, CSS, Jquery & Jquery UI.

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+$("#projects").tabs();
+$("ul").sortable({axis:"x", containment:"#projects"});
+$("ol").sortable({axis:"y", containment:"#projects"});
+$("#btnAddProject").button().click(function(){
+      $("#project-dialog").dialog({width: 400, resizable: false, modal: true,
+        buttons: {"Add new project": function(){
+                var projectName = $("#new-project").val();
+                $("<li><a href='#" + projectName + " '>" + projectName + "</a></li>")
+                .appendTo("#main");
+                $("<ol id=' " + projectName + " '></ol>").appendTo("#projects");
+                $("#projects").tabs("refresh");
+                var tabCount = $("#projects .ui-tabs-nav li").length;
+                $("#projects").tabs("option", "active", tabCount-1);
+                $("#new-project").val("");
+                $(this).dialog("close");
+
+        },
+      "Cancel": function(){
+              $("#new-project").val("");
+              $(this).dialog("close");
+      }
+
+
+    }});
+
+
+});
+
+
+
+
+
+});
